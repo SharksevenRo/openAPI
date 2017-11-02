6f3f27025d86ef3a79d3dbc99d7edbdbae94da3e
SharksevenRo <937257166@qq.com>
2017-11-02 20:24:47 +0800

    [update]

--- setup.sh
+++ setup.sh
@@ -1,7 +1,4 @@
-docker pull tutum/mongodb
-docker pull apinf/platform
+docker pull registry.cn-hangzhou.aliyuncs.com/sharkseven/openapi-hlg:latest
 echo www.openapi.dev 127.0.0.1 >> /etc/hosts
-docker run -d -p 27017:27017 -p 28017:28017 -e MONGODB_USER="openapi" -e MONGODB_DATABASE="openapi" -e MONGODB_PASS="openapi" tutum/mongodb
-
-docker run docker run -p 8754:80 --link=mongodb:mongodb -e MONGO_URL=mongodb://openapi:openapi@localhost:27017/openapi -e MAIL_URL=smtp://exmail.qq.com:25 -e ROOT_URL=http://www.openap.dev apinf/platform:latest
+docker run -dt -p 8754:80 --link=mongodb:mongodb -e DBPASS=openapi -e MONGO_URL=mongodb://admin:openapi@localhost:27017/openapi -e MAIL_URL=smtp://exmail.qq.com:25 -e ROOT_URL=http://www.openap.dev registry.cn-hangzhou.aliyuncs.com/sharkseven/openapi-hlg:latest
 
